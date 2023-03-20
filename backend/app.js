59896a8199a939aa7975778aaf5434a105f0a20e
Natasha J <natasha.jabury@gmail.com>
2023-03-20 09:27:42 +0100

    cloned the repository of the project with the front-end, I created a backend folder, installed npm (nodemon), the express framework and created an app.js file to compliment. middleware - .use and .get

--- backend/app.js
+++ backend/app.js
@@ -12,33 +12,19 @@ app.use((req, res, next) => {
 app.get('/api/sauces', (req, res, next) => {
     const stuff = [
       {
-        _id: 'string',
-        name: 'string',
-        manufacturer: 'string',
-        description: 'string',
+        _id: '123',
+        name: 'Cholula Original Hot Sauce, 5 fl oz        ',
+        manufacturer: 'Cholula',
+        description: 'This is where it all started. Cholula Original Hot Sauce is crafted from a generations-old family recipe that features arbol and piquin peppers and a blend of regional spices to deliver authentic Mexican flavor and heat. Taste the best life has to offer with Cholula Original Hot Sauce. With medium heat, the options are endless for this versatile sauce. Try on everything from eggs and rice & beans to beverages and even pizza! We love it on both traditional Mexican enchiladas and tamales as well as new favorites like burrito bowls and fish tacos. From our distinctive wooden cap to the original recipe hot sauce inside, every bottle of Cholula is a celebration of great food, flavor and our Mexican roots.        ',
         heat: 100,
         likes: 23,
         dislikes: 1,
-        imageUrl: 'string',
-        mainPepper: 'string',
-        usersLiked: 'string'[''],
-        usersDisliked: 'string'[''],
-        userId: 'string',
+        imageUrl: 'https://i5.walmartimages.com/asr/f0ef9049-d769-46cf-933b-5ce087095968.ae7c30538f20855df0fbbf37d7da80d8.jpeg',
+        mainPepper: 'Arbol & Piquin',
+        usersLiked: 'usersLiked'['me gusta'],
+        usersDisliked: 'usersDisliked'['no me gusta'],
+        userId: 'user123',
             },
-      {
-        _id: 'string',
-        name: 'string',
-        manufacturer: 'string',
-        description: 'string',
-        heat: 100,
-        likes: 23,
-        dislikes: 1,
-        imageUrl: 'string',
-        mainPepper: 'string',
-        usersLiked: 'string'[''],
-        usersDisliked: 'string'[''],
-        userId: 'string',
-      },
     ];
     res.status(200).json(stuff);
   });
