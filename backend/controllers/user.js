e4b03b1d4e0bea3f1c44c67b757bfd592d01c445
Natasha J <natasha.jabury@gmail.com>
2023-03-23 15:02:28 +0100

    web token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt= require('bcrypt');
 const User = require('./models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10).then(
@@ -40,9 +41,13 @@ exports.signup = (req, res, next) => {
                 error: new Error('Incorrect password!')
               });
             }
+            const token = jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' });
             res.status(200).json({
               userId: user._id,
-              token: 'token'
+              token: token
             });
           }
         ).catch(
