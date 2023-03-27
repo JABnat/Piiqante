9a2bf9ae9aeb5bf92a84fe6901e667d39e39787e
Natasha J <natasha.jabury@gmail.com>
2023-03-27 21:14:35 +0200

    multer-config

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const MIME_TYPES ={
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        const name = file.origionalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        const filename = name + Date.now() + '.' + extension;
+        callback(null, filename);
+    }
+});
+
+module.exports = multer({storage}).single('image');
